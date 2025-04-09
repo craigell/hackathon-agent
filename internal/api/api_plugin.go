c2621f0652131767506fff50c384ae92637f1a39
Aphral Griffin <a.griffin@f5.com>
2025-04-09 17:27:04 +0100

    merege

--- internal/api/api_plugin.go
+++ internal/api/api_plugin.go
@@ -53,7 +53,6 @@ func (a *AgentAPIPlugin) Info() *bus.Info {
 func (a *AgentAPIPlugin) Process(ctx context.Context, msg *bus.Message) {
 	switch msg.Topic {
 	case bus.InstanceHealthTopic:
-		a.test = append(a.test, "hello")
 		slog.InfoContext(ctx, "Received instance health event")
 		a.handleInstanceHealthTopic(ctx, msg)
 		slog.InfoContext(ctx, "Handled instance health event", "", a.healths)
